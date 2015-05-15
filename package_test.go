8af1cb5ff8c4dfc6bb8959672a317080db0b88d2
Joachim Lusiardi <joachim@lusiardi.de>
2015-05-15 15:11:12 +0000

    added function to create pointer to integers (used to create firewall policy rules

--- package_test.go
+++ package_test.go
@@ -7,19 +7,21 @@ import (
 
 func TestNew(t *testing.T) {
 	api := New("134", "abc")
-	
+
 	assert.Equal(t, "134", api.AuthToken)
 	assert.Equal(t, "abc", api.Endpoint)
 }
 
 func TestPrepareSession(t *testing.T) {
 	api := New("134", "abc")
+
 	session := api.prepareSession()
 	assert.Equal(t, "134", session.Header.Get("X_TOKEN"))
 }
 
 func TestCreateUrl_1(t *testing.T) {
 	api := New("token", "http://test.de/v1")
+
 	result := createUrl(api)
 	assert.Equal(t, "http://test.de/v1", result)
 }
@@ -36,4 +38,10 @@ func TestCreateUrl_3(t *testing.T) {
 
 	result := createUrl(api, "servers", 1)
 	assert.Equal(t, "http://test.de/v1/servers/1", result)
+}
+
+func TestInt2Pointer_1(t *testing.T) {
+	result := Int2Pointer(42)
+
+	assert.Equal(t, 42, *result)
 }
