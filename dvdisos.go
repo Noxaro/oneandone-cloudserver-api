8f9bba9f1a5e90b3f45497480de8fbb1c983048b
Joachim Lusiardi <joachim@lusiardi.de>
2015-05-21 14:41:20 +0000

    Change DVD isos to error handling code
    Extended the dvdisos.go file to use the error handling code to offer
    convenient handling of errors.

--- dvdisos.go
+++ dvdisos.go
@@ -2,6 +2,7 @@ package oneandone_cloudserver_api
 
 import (
 	log "github.com/Sirupsen/logrus"
+	"net/http"
 )
 
 type DvdIso struct {
@@ -16,25 +17,32 @@ type DvdIso struct {
 }
 
 // GET /dvd_isos
-func (api *API) GetDvdIsos() []DvdIso {
+func (api *API) GetDvdIsos() ([]DvdIso, error) {
 	log.Debug("requesting information about dvd isos")
 	session := api.prepareSession()
-	res := []DvdIso{}
-	resp, _ := session.Get(createUrl(api, "dvd_isos"), nil, &res, nil)
-	logResult(resp, 200)
-	for index, _ := range res {
-		res[index].api = api
+	result := []DvdIso{}
+	response, err := session.Get(createUrl(api, "dvd_isos"), nil, &result, nil)
+	if err := isError(response, http.StatusOK, err); err != nil {
+		return nil, err
+	} else {
+		for index, _ := range result {
+			result[index].api = api
+		}
+		return result, nil
 	}
-	return res
 }
 
 // GET /dvd_isos/{id}
-func (api *API) GetDvdIso(Id string) DvdIso {
+func (api *API) GetDvdIso(Id string) (*DvdIso, error) {
 	log.Debug("requesting information about dvd iso", Id)
 	session := api.prepareSession()
-	res := DvdIso{}
-	resp, _ := session.Get(createUrl(api, "dvd_isos", Id), nil, &res, nil)
-	logResult(resp, 200)
-	res.api = api
-	return res
+	result := new(DvdIso)
+	response, err := session.Get(createUrl(api, "dvd_isos", Id), nil, &result, nil)
+	if err := isError(response, http.StatusOK, err); err != nil {
+		return nil, err
+	} else {
+		result.api = api
+		return result, nil
+	}
+
 }
