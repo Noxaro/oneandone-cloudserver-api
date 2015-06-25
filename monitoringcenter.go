cabfc383faa7493b1d2b3814dd2fba7a16fa4093
Joachim Lusiardi <joachim.lusiardi@1und1.de>
2015-06-25 14:07:01 +0000

    Add new files for new resources in API.
    The API is going to be expanded by new resources. These file pave the
    way to the implementations of these resources.

---
+++ monitoringcenter.go
@@ -0,0 +1,11 @@
+/*
+ * Copyright 2015 1&1 Internet AG, http://1und1.de . All rights reserved. Licensed under the Apache v2 License.
+ */
+
+package oneandone_cloudserver_api
+
+import ()
+
+// GET /monitoring_center
+
+// GET /monitoring_center/{server_id}
---
+++ usages.go
@@ -0,0 +1,9 @@
+/*
+ * Copyright 2015 1&1 Internet AG, http://1und1.de . All rights reserved. Licensed under the Apache v2 License.
+ */
+
+package oneandone_cloudserver_api
+
+import ()
+
+// GET /usages
