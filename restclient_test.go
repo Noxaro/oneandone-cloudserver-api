3d31a3658be1455f85fd8b99b24fbeb431dacfc9
Joachim Lusiardi <joachim.lusiardi@1und1.de>
2015-07-15 13:35:15 +0000

    Add appendQueryParams to restclient.
    Add a function to append query parameters to a url.
    Separation of tests was also performed (split into restclient_test.go
    and package_test.go).

---
+++ restclient_test.go
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2015 1&1 Internet AG, http://1und1.de . All rights reserved. Licensed under the Apache v2 License.
+ */
+
+package oneandone_cloudserver_api
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestCreateUrl_1(t *testing.T) {
+	api := New("token", "http://test.de/v1")
+
+	result := createUrl(api)
+	assert.Equal(t, "http://test.de/v1", result)
+}
+
+func TestCreateUrl_2(t *testing.T) {
+	api := New("token", "http://test.de/v1")
+
+	result := createUrl(api, "servers")
+	assert.Equal(t, "http://test.de/v1/servers", result)
+}
+
+func TestCreateUrl_3(t *testing.T) {
+	api := New("token", "http://test.de/v1")
+
+	result := createUrl(api, "servers", 1)
+	assert.Equal(t, "http://test.de/v1/servers/1", result)
+}
+
+func TestAppendQueryParams_1(t *testing.T) {
+	params := map[string]interface{}{
+		"foo": "bar",
+	}
+	result := appendQueryParams("http://test/", params)
+	assert.Equal(t, "http://test/?foo=bar", result)
+}
+
+func TestAppendQueryParams_2(t *testing.T) {
+	params := map[string]interface{}{
+		"foo":  "bar",
+		"size": 5,
+	}
+	result := appendQueryParams("http://test/", params)
+	assert.Equal(t, "http://test/?foo=bar&size=5", result)
+}
+
+func TestAppendQueryParams_3(t *testing.T) {
+	params := map[string]interface{}{}
+	result := appendQueryParams("http://test/", params)
+	assert.Equal(t, "http://test/", result)
+}
